a11dbb0153752bd3cbedfb8a3e2aa9ac580ff86c
crazycodeboy <crazycodeboy@gmail.com>
2015-10-15 15:37:34 +0800

    add simple

--- simple/src/main/java/com/jph/simple/Test.java
+++ simple/src/main/java/com/jph/simple/Test.java
@@ -1,14 +1,18 @@
 package com.jph.simple;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import com.jph.putils.HttpUtil;
 import com.jph.putils.http.HttpRequest;
 import com.jph.putils.http.callback.RequestCallBack;
+import com.jph.putils.http.entity.HttpException;
+import com.jph.putils.http.entity.ResponseInfo;
+import com.ksudi.lib.android.login.KLogin;
 
-public class Test extends AppCompatActivity {
+public class Test extends Activity {
 
     private TextView textView;
 
@@ -22,13 +26,25 @@ public class Test extends AppCompatActivity {
         HttpUtil httpUtil=new HttpUtil();
         httpUtil.send(new HttpRequest("http://192.168.1.159/ksudi-star-api/android/client/expressGroup/searchExpressGroup/1",HttpRequest.HttpMethod.GET, null), new RequestCallBack() {
             @Override
-            public void onSuccess(String responseContent, String cookieStr) {
-                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+responseContent+" cookieStr:"+cookieStr);
+            public void onSuccess(ResponseInfo info) {
+                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+info.getResponseContent()+" cookieStr:"+info.getCookie());
             }
+
             @Override
-            public void onFailure(String error) {
-                textView.setText("time:"+System.currentTimeMillis()+"error:"+error);
+            public void onFailure(HttpException error) {
+                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+error.getResponseContent()+" cookieStr:"+error.getCookie()+" error:"+error.getErrorMsg());
             }
         });
+//        KLogin.login(this,"526971370@qq.com", "123456", new RequestCallBack() {
+//            @Override
+//            public void onSuccess(ResponseInfo info) {
+//                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+info.getResponseContent()+" cookieStr:"+info.getCookie());
+//            }
+//
+//            @Override
+//            public void onFailure(HttpException error) {
+//                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+error.getResponseContent()+" cookieStr:"+error.getCookie()+" error:"+error.getErrorMsg());
+//            }
+//        });
     }
 }
