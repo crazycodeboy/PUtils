7db93a8f4dae3eb767e9b6011da2d5edd34b1ec2
crazycodeboy <crazycodeboy@gmail.com>
2015-10-20 16:33:46 +0800

    登录模块测试通过

--- simple/src/main/java/com/jph/simple/Test.java
+++ simple/src/main/java/com/jph/simple/Test.java
@@ -4,27 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import com.jph.putils.HttpUtil;
+import com.jph.putils.http.HttpConfig;
 import com.jph.putils.http.HttpRequest;
 import com.jph.putils.http.callback.RequestCallBack;
 import com.jph.putils.http.entity.HttpException;
 import com.jph.putils.http.entity.ResponseInfo;
-//import com.ksudi.lib.android.login.KLogin;
+import com.ksudi.lib.android.login.KLogin;
 
 public class Test extends Activity {
 
     private TextView textView;
-
+    private String cookie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        textView= (TextView) findViewById(R.id.textView);
+        textView= (TextView) findViewById(R.id.test);
     }
-    public void onClick(View view){
+    public void login(View view){
+        KLogin.login(this,"526971370@qq.com", "123456", new RequestCallBack() {
+            @Override
+            public void onSuccess(ResponseInfo info) {
+                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+info.getResponseContent()+" cookieStr:"+info.getCookie());
+                cookie=info.getCookie();
+            }
+
+            @Override
+            public void onFailure(HttpException error) {
+                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+error.getResponseContent()+" cookieStr:"+error.getCookie()+" error:"+error.getErrorMsg());
+            }
+        });
+    }
+    public void access(View view){
         HttpUtil httpUtil=new HttpUtil();
-        httpUtil.send(new HttpRequest("http://192.168.1.159/ksudi-star-api/android/client/expressGroup/searchExpressGroup/1",HttpRequest.HttpMethod.GET, null), new RequestCallBack() {
+        httpUtil.send(new HttpRequest("http://192.168.1.18:1061/api/android/client/expressGroup/searchExpressGroup/1",HttpRequest.HttpMethod.GET, null,new HttpConfig().setCookie(cookie)), new RequestCallBack() {
             @Override
             public void onSuccess(ResponseInfo info) {
                 textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+info.getResponseContent()+" cookieStr:"+info.getCookie());
@@ -35,16 +49,19 @@ public class Test extends Activity {
                 textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+error.getResponseContent()+" cookieStr:"+error.getCookie()+" error:"+error.getErrorMsg());
             }
         });
-//        KLogin.login(this,"526971370@qq.com", "123456", new RequestCallBack() {
-//            @Override
-//            public void onSuccess(ResponseInfo info) {
-//                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+info.getResponseContent()+" cookieStr:"+info.getCookie());
-//            }
-//
-//            @Override
-//            public void onFailure(HttpException error) {
-//                textView.setText("time:"+System.currentTimeMillis()+" responseContent:"+error.getResponseContent()+" cookieStr:"+error.getCookie()+" error:"+error.getErrorMsg());
-//            }
-//        });
+    }
+
+    public void exit(View view){
+        KLogin.exit(new RequestCallBack() {
+            @Override
+            public void onSuccess(ResponseInfo info) {
+                textView.setText("注销成功："+info.getResponseContent());
+            }
+
+            @Override
+            public void onFailure(HttpException error) {
+                textView.setText("注销失败："+error.getErrorMsg());
+            }
+        });
     }
 }
